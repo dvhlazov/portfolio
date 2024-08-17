b63b4817d41853372c47a81780e4c9fdfa44eae3
dvhlazov <dvhlazov@gmail.com>
2024-08-17 21:41:47 +0300

    new

---
+++ http/public/script/main.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const links = document.querySelectorAll('.nav-link');
+    
+    links.forEach(link => {
+      link.addEventListener('click', function(event) {
+        
+        links.forEach(link => link.classList.remove('active'));
+  
+        this.classList.add('active');
+      });
+    });
+  });
